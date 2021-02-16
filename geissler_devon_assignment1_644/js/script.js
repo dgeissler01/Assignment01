41f4fd97b5241d12aea72cb576d03a954c63670e
dgeissler01 <78579118+dgeissler01@users.noreply.github.com>
2021-02-16 18:03:06 -0500

    Add files via upload

---
+++ geissler_devon_assignment1_644/js/script.js
@@ -0,0 +1,114 @@
+// STEP 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+// STEP 2
+// numeric literal     1
+// string literal      "1"
+// Boolean literal     true
+// null literal        null
+
+// STEP 3
+// let name = "Devon" + "Geissler";
+// let num = 1 + 1;
+
+// STEP 4
+// let strFirstName;
+// let strLastName;
+// let address;
+// let city;
+// let state;
+// let numZip;
+// let code;
+// let yourAge;
+// let referralSource;
+// let booleanMayWeContactYou;
+
+// STEP 5
+// let strFirstName = "Devon";
+// let numZio = 92107;
+// let booleanMayWeContactYou = false;
+
+// STEP 6
+// let address = 2882 + " Voltaire St San Diego, CA";
+// console.log(address);
+
+// STEP 7
+// let firstName = "Devon " + false;
+// let lastName = true + 10;
+// console.log(firstName);
+// console.log(lastName);
+
+// STEP 8
+// Not valid until added escape sequence \ before ' in I'm
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// window.console.log(someString);
+
+// STEP 9
+// let firstName = null;
+// let lastName;
+// console.log(firstName);
+// console.log(lastName);
+
+// STEP 10
+// console.log(typeof "hello");
+// console.log(typeof 5);
+// console.log(typeof false);
+// let firstName = new Object();
+// console.log(typeof firstName);
+// console.log(typeof lastName);
+
+// STEP 11
+// alert("Hello" + " Devon Geissler" + ", welcome to the JavaScript class!");
+
+// STEP 12
+// let name = "Devon Geissler";
+// alert("Hello " + name + ", welcome to the JavaScript class!");
+
+// STEP 13
+// let course = "Javascript";
+// alert("Hello" + " Devon Geissler" + ", welcome to the " + course + " class!");
+
+// STEP 14
+// alert("Hello" + " Devon Geissler" + ". \nWelcome to the JavaScript class!");
+
+// STEP 15
+// let name = prompt("What is your name?");
+// alert("Hello " + name + ", welcome to the JavaScript class!");
+
+// STEP 16
+// let course = prompt("What course will you be taking?");
+// alert("Hello" + " Devon Geissler" + ", welcome to the " + course + " class!");
+
+// STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
+// STEP 18
+// let x = 20;
+// x += 20;
+// console.log(x); 
+
+// STEP 19
+// let x = 20;
+// x *= 5;
+// console.log(x);
+
+// STEP 20
+// let x = 20 % 3;
+// x /= 1;
+// console.log(x);
+
+// STEP 21
+// let x = 10;
+// let y = 20;
+// console.log(y > x && x < y);
+
+// STEP 22
+// let x = 10;
+// let y = 20;
+// console.log(x != y || y != x);
